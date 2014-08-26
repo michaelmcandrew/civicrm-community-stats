4d0a8e999c562bc7fe9fc9df0caaf5962d030b02
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2014-08-26 17:58:26 +0100

    when populating ping table, ensure they are sorted by date so that we can easily identify latest ping

--- src/bin/import.sql
+++ src/bin/import.sql
@@ -7,10 +7,11 @@ INSERT INTO civicrm_stats.Site (hash)
 -- Add the ping backs
 INSERT INTO civicrm_stats.Ping (site_id, uf, language, country, version, ufversion, mysqlversion, phpversion, paymentprocessors, time, legacy_stat_id) 
     SELECT
-        Site.id, uf, lang, co, version, ufv, mysql, php, PPtypes, time, stats.id
+        Site.id, stats.uf, stats.lang, stats.co, stats.version, stats.ufv, stats.mysql, stats.php, stats.PPtypes, stats.time, stats.id
     FROM civicrm_stats.Site
     JOIN civicrm_stats_raw.stats
         ON Site.hash = stats.hash
+    ORDER BY stats.time
 ;
 
 -- Add the extensions
